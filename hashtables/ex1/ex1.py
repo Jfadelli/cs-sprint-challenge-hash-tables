3e743afa6143ffde41f3f13015995fa12b251a44
Jfadelli <jfadelli@gmail.com>
2020-10-09 19:23:04 -0700

    ex1_done

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,19 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    
+    #  dict that holds item weights
+    # dict that holds  
 
+    storage = {}
+
+    for i in range(length):
+        diff = limit - weights[i]
+        # print('diff -> ', diff)
+        current =  weights[i]
+        # print('weights -> ', weights)
+        if diff in storage:
+            return (i, storage[diff])
+        else:
+            storage[current] = i
     return None
